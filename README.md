5a7eeef96334221a512c744a12551e5dc70b3d50
TimoZhang <ztnuaa0211@gmail.com>
2018-03-20 00:27:15 +0800

    basic function complete!

---
+++ README.md
@@ -0,0 +1,10 @@
+ssr-qrgen
+=================
+This project was based on [shadwosocks-qrgen](http://phoenixlzx.github.io/shadowsocks-qrgen) by [phoenixlzx](https://github.com/phoenixlzx).  
+
+What I did is modified it to support shadowsocksR QR code generation.  
+
+Modified: app.js and index.html   
+
+Todo:
+Need to reload the page to generate the new QRcode if you change the config.
---
+++ app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var qrcode = new QRCode("qrcode");
+
+function manipulate() {
+	var ip = $("#server-address").val();
+	var port = $("#server-port").val();
+	var password = $("#password").val();
+	var encryption = $("#encryption").val();
+	var protocol = $("#protocol").val();
+	var prot_param = $("#prot_param").val();
+	var obfs = $("#obfs").val();
+	var obfs_param = $("#obfs_param").val();
+	var remarks =  $("#remarks").val();
+	var group =  $("#group").val();
+
+	if (ip && port && port > 0 && password && encryption) {
+		// The ssr QR code scheme can be found at 
+		// https://github.com/shadowsocksrr/shadowsocks-rss/wiki/SSR-QRcode-scheme
+		// Url safe base64 without padding
+		var bs64pass = utf8_to_b64(password);
+		    bs64pass = bs64pass.replace(/=+/,"");
+		var bs64obfs_param = utf8_to_b64(obfs_param);
+		    bs64obfs_param = bs64obfs_param.replace(/=+/,"");
+		var bs64prot_param = utf8_to_b64(prot_param);
+		    bs64prot_param = bs64prot_param.replace(/=+/,"");
+		var bs64remarks = utf8_to_b64(remarks);
+		    bs64remarks = bs64remarks.replace(/=+/,"");
+		var bs64group = utf8_to_b64(group);
+		    bs64group = bs64group.replace(/=+/,"");
+
+		var ssr=ip+":"+port+":"+protocol+":"+encryption+":"+obfs+":"+bs64pass+"/?obfsparam="+bs64obfs_param+"&protoparam="+bs64prot_param+"&remarks="+bs64remarks+"&group="+bs64group;
+		var bs64ssr = utf8_to_b64(ssr);
+			bs64ssr=bs64ssr.replace(/=+/,"");
+			bs64ssr=bs64ssr.replace(/\//,"_");
+
+		qrcode.makeCode('ssr://' + bs64ssr);
+	} else {
+		alert("Please fill the needed blanks.");
+	}
+}
+
+function utf8_to_b64(str) {
+	return window.btoa(unescape(encodeURIComponent(str)));
+}
+
+$("#generate").click(function(event){
+	event.preventDefault();
+	manipulate();
+});
